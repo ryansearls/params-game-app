ed195e60b3334560c4d25bd07daf8c268578d46b
Ryan Searls <Ryan.searls@gmail.com>
2021-06-17 13:04:25 -0500

    added routes/controller

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,2 +1,18 @@
 class ParamsController < ApplicationController
+
+def name
+  input_value = params["message"]
+  if input_value[0].upcase == "A"
+  render plain: {message: " #{input_value} your name starts with A!"}
+  begin
+    render json: {name: }
+  rescue => exception
+    
+  else
+    
+  ensure
+    
+  end
+end 
+
 end

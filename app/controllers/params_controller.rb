a59057b01df38b9fc7bb1d57b20dcbf093347444
Ryan Searls <Ryan.searls@gmail.com>
2021-06-18 11:32:42 -0500

    added guess number game

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -4,15 +4,25 @@ def name
   input_value = params["message"]
   if input_value[0].upcase == "A"
   render plain: {message: " #{input_value} your name starts with A!"}
-  begin
-    render json: {name: }
-  rescue => exception
-    
   else
+    render json: {message: "Your name does not start with A!"} 
+  end 
+end   
     
-  ensure
-    
-  end
-end 
+# def new_params
+  
 
+  
+
+def random_number
+  answer = 15
+  input_value = params["number"].to_i
+  if input_value > 15
+    output_message = "That's too high"
+  elsif input value < 15
+    output_message = "That's too low"
+  else 
+    output_message "That's correct"
+  end 
+    render json: {message: output_message}    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
 
  get "/name", controller: "params", action: "name"
 
+ get "/guess", controller: "params", action; "guess"
 
+get "/segment_params/:key" => "params#new_params"
+
+
+post "/random_number", controller: "params", action: "random_number"
+
+# post ""
 
 end
